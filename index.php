519fb7d56e15521646f7cf455de14ce26f803db3
GatiyatovDD <159507057+GatiyatovDD@users.noreply.github.com>
2024-04-28 14:34:22 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,50 @@
 <?php
+date_default_timezone_set('Asia/Yekaterinburg');
 require "dbconnect.php";
+require "auth.php";
+require "menu.php";
+echo '<main class="container" style="margin-top: 100px">';
+switch ($_GET['page']){
+    case 'c':
+        if (isset($_SESSION['login'])) {
+            require "transport.php";
+        }
+        else{
+            $msg = 'Войдите в систему для просмотра транспорта';
+        }
+        break;
+    case 'v':
+        if (isset($_SESSION['login'])) {
+            require "route.php";
+        }
+        else{
+            $msg = 'Войдите в систему для просмотра доступных маршрутов';
+        }
+        break;
+    case 'b':
+        if (isset($_SESSION['login'])) {
+            require "cargo.php";
+        }
+        else{
+            $msg = 'Войдите в систему для просмотра и добавления Заказов (грузов)';
+        }
+        break;
+    case 't':
+        if (isset($_SESSION['login'])){
+            require "tasks.php";
+            require "taskform.php";
+        }
+        else{
+            $msg = 'Войдите в систему для просмотра и создания задач';
+        }
+        break;
+}
+echo '</main>';
 
-if ($conn->connect_error) {
-    die("Ошибка: невозможно подключиться: " . $conn->connect_error);
+if(($_SESSION['msg']!='') or isset($msg)) {
+    require "message.php";
+    $_SESSION['msg']= '';
 }
 
-echo 'Подключились к базе.<br>';
-
-$result = $conn->query("SELECT * FROM cargo") ;
-echo "<h2>Таблица cargo</h2>";
-echo 'ID'. ' '. 'ID_Рейса'. ' '. 'Вес'. ' '. 'Отправитель'."<br>";
-while ($row = $result->fetch()) {
-    echo $row['ID']. ' '. $row['ID_Рейса']. ' '. $row['Вес']. ' '. $row['Отправитель']."<br>";
-
-}
+require "footer.php";
+?>
